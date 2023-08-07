c23512dc92ba0bf758a05ac2ac06627ebca59171
davidOlea <luis.david.olea.mtz@gmail.com>
2023-08-07 16:13:33 -0600

    ci: funciones expresadas

--- assets/pages/sesion-js/js02.js
+++ assets/pages/sesion-js/js02.js
@@ -14,3 +14,35 @@ console.log( multiplica(4, 3));
 function multiplica(a, b){
     return a * b;
 }
+
+/*
+ ------------ Funciones expresadas -----------------------
+            (function expressions)
+Son funciones declaradas dentro de la asignación de una variable.
+Estas funciones pueden ser anónimas ( no tienen nombre ).
+Las funciones expresadas no tiene hoisting, porque no se
+carga en memoria hasta que se utilice.
+
+sintaxis:
+    const nombreVariable = function nombreFuncion (parámetros){
+        instrucciones;
+    };
+*/
+
+const sum = function sumatoria(a, b){
+    return a + b;
+}
+console.log(`El resultado de 4 + 2 : ${ sum(4,2) }`);
+
+let saludo; // undefined
+const localidad = "tlaxcala";
+
+if ( localidad == "buenos aires"){
+    saludo = function ( name ) { return `Holis ${name}`};
+}
+else {
+    saludo = function ( name ) { return `Buenos días ${name} `};
+}
+
+console.log( saludo("Sergio"));
+console.log( saludo("David"));
