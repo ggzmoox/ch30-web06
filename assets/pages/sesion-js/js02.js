297ceeaa6bf542d6f2df23ab17d206449f72962b
davidOlea <luis.david.olea.mtz@gmail.com>
2023-08-07 14:35:16 -0600

    ci: funciones declaradas

---
+++ assets/pages/sesion-js/js02.js
@@ -0,0 +1,16 @@
+console.log("Sesion JS02 Uso de Funciones");console.log("Sesión JS02 Uso de funciones");
+
+/*
+  ----------- Funciones declaradas --------------
+    (function declaration, function statement)
+Una característica de las funciones declradas es que tien hoisting.
+
+hoisting: comportamiento en JS que permite a las declaración de variables(var)
+o funciones se eleven al comienzo de su ámbito antes de que se ejecute el código.
+
+*/
+console.log( multiplica(4, 3));
+
+function multiplica(a, b){
+    return a * b;
+}
