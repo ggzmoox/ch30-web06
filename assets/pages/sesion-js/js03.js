bf40fe712a691701e2549f5a735296b382e29987
davidOlea <luis.david.olea.mtz@gmail.com>
2023-08-09 12:52:48 -0600

    ci: Solucion ej 4 ventilador parse int

--- assets/pages/sesion-js/js03.js
+++ assets/pages/sesion-js/js03.js
@@ -50,3 +50,113 @@ console.log(`${firstname} ${lastname}`); // sergio torres
 console.log(age); // 26
 //console.log( colorFavorito ); // colorFavorito no está definido
 console.log(comidaFavorita); // pozole
+
+// ----------------- Condicional if-else --------------------
+/*
+ Ejecuta una sentencia si una condición específica es
+ evaluada como verdadera.
+
+ Sintaxis:
+    if ( condición ) sentencia;
+
+    if ( condición ) {
+        sentencias;
+    }
+
+    if ( condición ) sentencia;
+    else sentencia_si_condición_es_falsa;
+
+    if ( condición ) {
+        sentencias;
+    }
+    else {
+        sentencias;
+    }
+
+    if (condición) sentencia;
+    else if (condicion 2) sentencia;
+         else sentencia_si_condicion2_es_falsa;
+
+
+    if ( condicion1 ) sentencias;
+    else if ( condicion2 ) sentencia;
+    else if ( condicion3 ) sentencia;
+    else if ( condicion4 ) sentencia;
+     .....
+    else if ( condicionN ) sentencia;
+    else condición;   
+
+*/
+
+const temperatura = 25;
+let mensaje = "Temperatura de ";
+
+if ( temperatura === 22 ){
+    mensaje += `${temperatura} grados centigrados es ideal` ; // mensaje = mensaje + nvoTexto
+}
+else if ( temperatura >= 15 && temperatura<= 21) {
+    mensaje += `${temperatura} grados centigrados es fría` ;
+}
+else if( temperatura>=23 && temperatura <= 30 ){
+//else if( temperatura <= 30 &&  temperatura>=23 ){
+    mensaje += `${temperatura} grados centigrados es calurosa`;
+}
+else {
+    mensaje = `${temperatura} grados centigrados no es ideal =(` ;    
+}
+
+console.log(mensaje);
+
+//--------------- Condicional Switch --------------------------
+/*
+ La condicional Switch evalua una expresión y se compara
+ con el valor de cada instancia en 'case' y se ejecuta las
+ sentencias asociadas a ese 'case' hasta encontrar
+ la sentencia 'break' o se finalicen todas las sentencias
+ de la condicional switch.
+
+ Sintaxis:
+  switch (expresión) {
+    case valor1:
+        sentencia;
+        break;
+    case valor 2:
+        sentencias;
+        break;
+    case valor n:
+        sentencias;
+        break
+    default:
+        sentencias;
+        break;
+  }
+
+*/
+
+/**
+ * Establece la velocidad de un ventilador
+ * @param {number} velocidad del ventilador
+ * @return {number} mensaje de la velocidad establecida
+ */
+const setVelocidadVentilador = ( velocidad = 0 )=>{
+    let mensaje;
+    // la condicional switch utiliza la comparación estricta ( === )
+    switch ( parseInt(velocidad) ) {
+        case 0:
+            mensaje = "apagado"; break;
+        case 1:
+            mensaje = "velocidad baja"; break;
+        case 2:
+        case "2":
+            mensaje = "velocidad media"; break;
+
+        default:
+            mensaje = "el nivel no existe"; break;
+    }
+    return mensaje;
+}
+console.log(`La velocidad del ventilador está en ${setVelocidadVentilador(1)}`); // baja
+console.log(`La velocidad del ventilador está en ${setVelocidadVentilador(8)}`); // no existe
+console.log(`valor: 2 ${setVelocidadVentilador(2)}`); // velocidad media
+console.log(`valor: "2" ${setVelocidadVentilador("2")}`); // velocidad media
+console.log(`valor: ? ${setVelocidadVentilador()}`); // apagado
