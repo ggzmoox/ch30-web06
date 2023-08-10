95bca7b4b88e6ffbb01b3ffb7c9b7847a615fb46
davidOlea <luis.david.olea.mtz@gmail.com>
2023-08-10 11:15:22 -0600

    ci: uso de for, for-of, for-each

--- assets/pages/sesion-js/js04.js
+++ assets/pages/sesion-js/js04.js
@@ -27,3 +27,55 @@ for (let equipo = 0; equipo < personasEnCh30.length /* filas */; equipo++) {
     }
     
 }
+
+// ----------------------- Uso de for... of ------------------
+/* Ejecuta una sentencia por cada elemento de un objeto iterable(array, colección, string).
+Sintaxis:
+    for (const iterator of object) {
+    
+    }
+*/
+console.table( personasEnCh30 ); 
+
+const myPet = "Kraken";
+for (const character of myPet  ){
+    console.log( character );
+}
+myPet.split("").forEach( character => console.log( character ))
+
+
+for (const equipo of personasEnCh30 ) {
+    console.log( equipo ); // Arreglo de equipos
+    for (const persona of equipo) {
+        console.log( persona ); //  Persona de cada equipo
+    }
+}
+
+// ---------------- forEach -------------------------
+/*
+ Método que se utiliza para iterar colecciones, arreglos.
+ Permite ejecutar una función por cada elemento del arreglo.
+
+*/
+console.table( personasEnCh30 ); 
+
+function iterarEquipos( equipo, indice, arreglo ){
+    console.log(`Indice ${indice}: ${equipo} `)
+    equipo.forEach(  iterarPersonas  );
+    return equipo; // arreglo de cada equipo integrador
+}
+
+function iterarPersonas( persona, indice ){
+    console.log(`Indice P ${indice}: ${persona} `)
+    return persona;
+}
+
+
+// personasEnCh30.forEach( iterarEquipos );
+
+personasEnCh30.forEach( (equipo, indexEquipo) => 
+                    equipo.forEach( (persona, indexPer)=> 
+                    console.log(` [${indexEquipo}][${indexPer}] : ${persona}` )));
+ 
+// Solo se imprimi el elemento (persona)                    
+//personasEnCh30.forEach( equipo => equipo.forEach( persona=> console.log(`${persona}`)));
