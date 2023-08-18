33b51dc6e8f0e3d77e2c98876265f9410fe26e67
davidOlea <luis.david.olea.mtz@gmail.com>
2023-08-18 11:32:00 -0600

    creacion archivo product class

--- assets/pages/sesion-js/js08-clases.js
+++ assets/pages/sesion-js/js08-clases.js
@@ -1,33 +1,21 @@
+import { Products } from "./js08-product-class.js";
+
 console.log("Sesión Js08- class");
 
-const getProducts = async() => {
-    const url = "https://fakestoreapi.com/products";
+const getProducts = async(url = "https://fakestoreapi.com/products" ) => {
+    // const url = "https://fakestoreapi.com/products";
     //const url = "https://reqres.in/api/users?page=2";
 
     const responseJSON = await fetch(url);
     const response = await responseJSON.json();
     console.log( response)
+    return response;
 }
 
 /**
- * Clase productos
- * El nombre de las clases se realizancon UpperCamelCase
+ * Creación de arreglode productos con la clase Products
+ * @returns 
  */
-class Products{
-    // El método constructor nos ayuda a instanciar un objeto
-    constructor( id, name){
-        this.name = name; // creando el atributo name y le asignamos el valor de parámetro name
-        this.id = id;
-        this.createdAt = new Date().getTime();
-        // console.log(`Producto ${this.name} se creó el ${new Date().toLocaleString()}`);
-    }
-
-    lifeSpan(){
-        return new Date().getTime() - this.createdAt;
-    }
-
-}
-
 function createProductsOfClassProducts(){
              // Instanciar la clase Products para crear un objeto
  const zote = new Products(1,"Zote");
@@ -45,7 +33,12 @@ function createProductsOfClassProducts(){
  return products;
 }
 
-function lifeSpanProducts( products ) {
+/**
+ * Crea list Items de un arreglo de productos
+ * @param {array} products 
+ * @returns 
+ */
+function createListItemsOfProducts( products ) {
     // El método map sobre un arreglo, itera sobre cada elemento
     // del arreglo y entrega un nuevo arreglo.
     const productsLifeSpan = products.map( product => 
@@ -63,8 +56,60 @@ function insertListItems( listItems ){
 
 function onClickLifeSpan(){
     const products = createProductsOfClassProducts(); // Crea objetos
-    setTimeout( ()=>{
-        const productsListItems = lifeSpanProducts ( products );
-        insertListItems( productsListItems );
+    setTimeout( ()=>{ // crea retardo de 5 ms para ver la diferencia de tiempo.
+        const productsListItems = createListItemsOfProducts( products ); // Crea <li>
+        insertListItems( productsListItems ); // insertar en el DOM
     } , 5000);
+}
+
+
+//==============================================================
+const refShowProducts = document.getElementById("show-products");
+refShowProducts.addEventListener( "click", ()=>{
+    showProducts();
+});
+
+async function showProducts(){
+    // const products = createProductsOfClassProducts();
+    // const products = await createProductsOfFakeStore();
+    const products = await createProductsOfTapioca();
+    console.table(products);
+    const productCards = createCardsOfProducts( products);
+    insertCards( productCards );
+}
+
+async function createProductsOfFakeStore(){
+    const fakeProducts = await getProducts();
+    //return fakeProducts.maps( (fakeProduct)=> new Products( fakeProduct.id , fakeProduct.title));
+    return fakeProducts.map( ({id, title})=> new Products( id , title));
+}
+
+async function createProductsOfTapioca(){
+    const fakeProducts = await getProducts("/assets/json/tapioca.json");    
+    return fakeProducts.map( ({serie, nombre, image })=> new Products( serie , nombre , image || null));
+}
+
+
+function createCardsOfProducts( products){
+    return products.map( (product)=>{
+        return `
+        <div class="card col-4 m-3" style="width: 18rem;">
+        <img src="${product.image}" class="card-img-top my-2" alt="tapioca">
+        <div class="card-body">
+          <h5 class="card-title">${ product.name}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">${ product.id}</h6>
+          <p class="card-text">Some title content pon uwu xd :V</p>
+          <a href="#" class="card-link">Card link</a>
+        </div>
+      </div>       
+        `
+    } );
+}
+
+
+function insertCards( productCards ) {
+    const products = document.getElementById("products");
+    const cards = `<div class="row">  ${productCards.join("")}  </div>`;
+
+    products.innerHTML = cards;
 }
